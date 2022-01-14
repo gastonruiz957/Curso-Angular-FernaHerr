d2c07789769be06fe13ea1b315057d07fd278f04
gastonruiz957 <39232205@fi.unju.edu.ar>
2022-01-14 18:50:05 -0300

    mi primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+ import { Component } from '@angular/core';
+import { Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: 'app.component.html'
 })
 export class AppComponent {
-  title = 'bases';
+ 
+
+
+
+
+
+// sumar () {
+//   this.numero += 1;
+// }
+
+// restar () {
+//   this.numero -= 1;
+// }
+
 }
